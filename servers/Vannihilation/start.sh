4d26ac5bf7028aaeca537fdfbb0257c835753e63
scherepanov <sergey@cherepanov.org.ua>
2021-04-13 01:07:12 +0400

    plugins

--- servers/Vannihilation/start.sh
+++ servers/Vannihilation/start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
-cd ${DIR};
-docker stop Vannihilation
-docker rm Vannihilation
 
-git reset --hard && git clean -fd && git pull
-rm -rf worlds/*
-./start.sh
+cd ${DIR}/config && rsync -a --exclude-from='exclude.txt' ../../../clients/Vannihilation/config/ ./ && cd -
+cd ${DIR}/mods && rsync -a --exclude-from='exclude.txt' ../../../clients/Vannihilation/mods/ ./ && cd -
+
+MC_CPU_COUNT=${MC_CPU_COUNT-4} MC_MAX_MEMORY=${MC_MAX_MEMORY-8192M} MC_INIT_MEMORY=${MC_INIT_MEMORY-4096M} mc-docker-run --auth-server-url https://auth.craftorio.com \
+--image craftorio/docker-server-minecraft:1.16.5-457-mohist \
+--name Vannihilation \
+--data "${DIR}" \
+--port 25566

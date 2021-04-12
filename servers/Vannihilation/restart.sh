104af54d5c6a47742ae509c598165274cb641ad6
scherepanov <sergey@cherepanov.org.ua>
2021-04-12 17:21:13 +0400

    restart script

--- servers/Vannihilation/restart.sh
+++ servers/Vannihilation/restart.sh
@@ -2,7 +2,7 @@
 
 pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 cd ${DIR};
-git reset --hard && git pull
+git reset --hard && git clean -fd && git pull
 
 docker stop Vannihilation
 docker rm Vannihilation

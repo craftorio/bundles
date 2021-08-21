8fa33ca4dd15c302fab71f8c90a2c5c8df5be366
scherepanov <sergey@cherepanov.org.ua>
2021-08-22 01:12:57 +0400

    update start script

--- servers/Vannihilation/start.sh
+++ servers/Vannihilation/start.sh
@@ -2,17 +2,16 @@
 set -x
 set -e
 cd $(dirname $0);
+export MC_PROJECT_NAME=$(basename $(pwd))
 
 export MINECRAFT_AUTH_SERVER_URL=${MINECRAFT_AUTH_SERVER_URL-"https://auth.craftorio.com"}
 export CONTAINER_IMAGE=${CONTAINER_IMAGE-"craftorio/docker-server-minecraft:mohist-1.16.5-759"}
 export MC_CPU_COUNT=${MC_CPU_COUNT-4}
 export MC_INIT_MEMORY=${MC_INIT_MEMORY-2048M}
 export MC_MAX_MEMORY=${MC_MAX_MEMORY-8192M}
-export MC_PROJECT_NAME=$(basename $(pwd))
 export MC_PORT=${MC_PORT-25566}
-
 
 cd ${DIR}/config && rsync -a --exclude-from='exclude.txt' ../../../clients/Vannihilation/config/config/ ./ && cd -
 cd ${DIR}/mods && rsync --delete -a --exclude=exclude.txt --exclude-from='exclude.txt' ../../../clients/Vannihilation/mods/ ./ && cd -
 
-mc-docker-run --name ${PROJECT_NAME} --data $(pwd) --port 25566
+mc-docker-run --name ${MC_PROJECT_NAME} --data $(pwd) --port 25566

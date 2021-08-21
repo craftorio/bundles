ffc014100384418319e05df51344b8c77e1cf553
scherepanov <sergey@cherepanov.org.ua>
2021-08-22 01:22:12 +0400

    update start script

--- servers/Vannihilation/start.sh
+++ servers/Vannihilation/start.sh
@@ -14,4 +14,6 @@ export MC_PORT=${MC_PORT-25566}
 cd ${DIR}/config && rsync -a --exclude-from='exclude.txt' ../../../clients/Vannihilation/config/config/ ./ && cd -
 cd ${DIR}/mods && rsync --delete -a --exclude=exclude.txt --exclude-from='exclude.txt' ../../../clients/Vannihilation/mods/ ./ && cd -
 
-mc-docker-run --name "${MC_PROJECT_NAME}" --data "$(pwd)" --port ${MC_PORT}
+mc-docker-run --name "${MC_PROJECT_NAME}" --data "$(pwd)" --port ${MC_PORT} \
+--image "${CONTAINER_IMAGE}" \
+--auth-server-url "${MINECRAFT_AUTH_SERVER_URL}"

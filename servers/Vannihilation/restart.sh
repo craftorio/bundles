591c3cdba1528f9e6a520b15b8395802dc7bf6e1
scherepanov <sergey@cherepanov.org.ua>
2021-08-22 01:15:51 +0400

    update start script

--- servers/Vannihilation/restart.sh
+++ servers/Vannihilation/restart.sh
@@ -4,7 +4,7 @@ set -e
 cd $(dirname $0);
 export MC_PROJECT_NAME=$(basename $(pwd))
 
-if [[ docker ps -a | grep "${MC_PROJECT_NAME}" ]]
+if docker ps -a | grep "${MC_PROJECT_NAME}"; then
   docker stop "${MC_PROJECT_NAME}"
   docker rm "${MC_PROJECT_NAME}"
 fi

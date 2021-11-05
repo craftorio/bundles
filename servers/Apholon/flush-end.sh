b023e1b1645f7807f6943d9fc103fa93b374d106
scherepanov <sergey@cherepanov.org.ua>
2021-11-05 08:23:56 +0400

    autoscripts

--- servers/Apholon/flush-end.sh
+++ servers/Apholon/flush-end.sh
@@ -9,4 +9,5 @@ sed -i 's/Byte "DragonKilled" 0/Byte "DragonKilled" 1/g' worlds/world/level.txt
 sed -i 's/Byte "PreviouslyKilled" 0/Byte "DragonKilled" 1/g' worlds/world/level.txt
 sed -i 's/Byte "LegacyScanPerformed" 1/Byte "DragonKilled" 0/g' worlds/world/level.txt
 nbted -r worlds/world/level.txt > worlds/world/level.dat
+rm -rf worlds/world/DIM1
 ./start.sh

d3f5671d04a6890e1c1d34d7173d8f06f3bc1d3f
scherepanov <sergey@cherepanov.org.ua>
2021-11-05 08:22:08 +0400

    autoscripts

---
+++ servers/Apholon/crop-worlds.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -x
+set -e
+REGION_MAX_DISTANCE=4
+cd $(dirname $0);
+[[ -f .env ]] || { echo "No .env file found, terminating..."; exit 1; }
+./stop.sh
+nbted --print worlds/world/level.dat > worlds/world/level.txt
+cd worlds/world/dimensions/twilightforest/twilightforest/region
+ls | awk -F\. '{ if ($2 < -'${REGION_MAX_DISTANCE}' || $3 < -'${REGION_MAX_DISTANCE}' || $2 > '${REGION_MAX_DISTANCE}' || $3 > '${REGION_MAX_DISTANCE}') { print $0; }  }' | xargs rm
+cd -
+
+cd worlds/world/dimensions/twilightforest/skylight_forest/region
+ls | awk -F\. '{ if ($2 < -'${REGION_MAX_DISTANCE}' || $3 < -'${REGION_MAX_DISTANCE}' || $2 > '${REGION_MAX_DISTANCE}' || $3 > '${REGION_MAX_DISTANCE}') { print $0; }  }' | xargs rm
+cd -
+
+cd worlds/world/dimensions/rats/ratlantis/region
+ls | awk -F\. '{ if ($2 < -'${REGION_MAX_DISTANCE}' || $3 < -'${REGION_MAX_DISTANCE}' || $2 > '${REGION_MAX_DISTANCE}' || $3 > '${REGION_MAX_DISTANCE}') { print $0; }  }' | xargs rm
+cd -
+
+cd worlds/world/DIM-1/region
+ls | awk -F\. '{ if ($2 < -'${REGION_MAX_DISTANCE}' || $3 < -'${REGION_MAX_DISTANCE}' || $2 > '${REGION_MAX_DISTANCE}' || $3 > '${REGION_MAX_DISTANCE}') { print $0; }  }' | xargs rm
+cd -
+
+./start.sh
--- servers/Apholon/restart.sh
+++ servers/Apholon/restart.sh
@@ -3,16 +3,7 @@ set -x
 set -e
 cd $(dirname $0);
 [[ -f .env ]] || { echo "No .env file found, terminating..."; exit 1; }
-source .env
-MC_PROJECT_NAME=${MC_PROJECT_NAME-$(basename $(pwd))}
 
-if docker ps | grep "${MC_PROJECT_NAME}"; then
-  docker exec -it "${MC_PROJECT_NAME}" minecraft stop
-  docker stop "${MC_PROJECT_NAME}"
-fi
-
-docker rm "${MC_PROJECT_NAME}"
-
+./stop.sh
 git pull
-
 ./start.sh

eee62b80e5fbe4b398c6963fb0f7143727ac01a6
scherepanov <sergey@cherepanov.org.ua>
2021-11-07 04:56:58 +0400

    - Rpg_gravestone-1.16.5-1.0.1.jar

--- servers/Apholon/start.sh
+++ servers/Apholon/start.sh
@@ -8,7 +8,8 @@ MC_PROJECT_NAME=${MC_PROJECT_NAME-$(basename $(pwd))}
 
 cd config && rsync -a --exclude=exclude.txt --exclude-from='exclude.txt' ../../../clients/${MC_PROJECT_NAME}/config/config/ ./; cd -
 cd mods && rsync --delete -a --exclude=exclude.txt --exclude-from='exclude.txt' ../../../clients/${MC_PROJECT_NAME}/mods/ ./; cd -
-cd worlds/world/datapacks && rsync --delete -a ../../../datapacks ./; cd -
+mkdir -p worlds/world/datapacks
+cd worlds/world/datapacks && rsync --delete -a ../../../datapacks/ ./; cd -
 
 mc-docker-run --name "${MC_PROJECT_NAME}" --data "${MC_DATA_DIR-$(pwd)}" --port ${MC_PORT} \
 --image "${CONTAINER_IMAGE}" \

f96ddf9cbb1ead160f6ff69a90487977b23cdf7e
scherepanov <sergey@cherepanov.org.ua>
2024-02-20 10:55:58 +0000

    libs

--- servers/Mododolon/flush-nether.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-cd $(dirname $0);
-[[ -f .env ]] || { echo "No .env file found, terminating..."; exit 1; }
-./stop.sh
-rm -rf worlds/world/DIM-1
--- servers/MododolonPTR/restart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-cd $(dirname $0);
-[[ -f .env ]] || { echo "No .env file found, terminating..."; exit 1; }
-
-./stop.sh
-./start.sh
